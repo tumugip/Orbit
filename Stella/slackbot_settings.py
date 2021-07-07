aacad1b6a19f38680d8120bf1d60ee9b09fdcccf
Shiho Takano <collectingsamidare@gmail.com>
2021-07-07 23:11:26 +0900

    add Ver.3.0 UNIFIED THE EXPRESSIONS

--- Stella/slackbot_settings.py
+++ Stella/slackbot_settings.py
@@ -1,6 +1,6 @@
 
 API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-default_reply = "ちょっとよく分からなかったです。ごめんなさい。"
+default_reply = "ちょっとよく分からなかったんだゾ、別の言い回しをしてみて欲しいゾ！"
 
 # 機能追加のコードを書いたディレクトリ名を入れる
 PLUGINS = ['plugins']
