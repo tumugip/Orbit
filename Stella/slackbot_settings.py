cc7f98e75c08df9739bb46f8ac2e10e1cb0a4945
shio <shiori.4211@gmail.com>
2021-07-08 22:28:47 +0900

    update Stella

--- Stella/slackbot_settings.py
+++ Stella/slackbot_settings.py
@@ -1,5 +1,5 @@
 
-API_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+API_TOKEN = "xoxb-2161681480723-2190818312323-I1IAdhgRxSkKNFv3KJgntN1Y"
 default_reply = "ちょっとよく分からなかったです。ごめんなさい。"
 
 # 機能追加のコードを書いたディレクトリ名を入れる
